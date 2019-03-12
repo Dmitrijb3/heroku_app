b2d48edce972ed4d921a22e59670a35c1ea7592c
D1m0n <36866808+Dmitrijb3@users.noreply.github.com>
2019-03-12 18:21:42 +0200

    Changes
    Changes step 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
@@ -19,6 +19,20 @@ def portfolio():
 @app.route("/contacts")
 def contacts():
     return render_template('contacts.html')
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    cookie_name = request.cookies.get("user_name") # Переделать !!!!
+    contact_name = request.form.get("name")
+    result = None
+    if contact_name:
+        print('No klienta atnaca: {}'.format(contact_name))
+        result = contact_name.upper()
+    return render_template('login.html', result=result, login_name=cookie_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
